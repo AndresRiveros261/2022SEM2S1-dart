4ce8e58a26d930f6a8135233248726263c01cf2c
Andres Ricardo Riveros <104392170+AndresRiveros261@users.noreply.github.com>
2022-09-09 16:40:57 -0500

    Update ejercicios.dart

--- 7.ejercicios/ejercicios.dart
+++ 7.ejercicios/ejercicios.dart
@@ -23,8 +23,25 @@ print(listaFinal);
 
 }
 
+//ejercicio 1
 
 var lista = List.of({1, 4, 9, 16, 25, 36, 49, 64, 81, 100});
 var listapar = lista.where((number) => number.isEven);
 print(listapar);
 
+
+// ejercicio 2
+
+var lista1 = List.of({1,1,2,3,5,8,13,21,34,55,89});
+var lista2 = List.of({1,2,3,4,5,6,7,8,9,10,11,12,13});
+var lista3 = [];
+
+  for (var numero in lista1) {
+    if (lista2.contains(numero)) {
+      if (!lista3.contains(numero)) {
+        lista3.add(numero);
+      }
+    }
+  }
+
+  print(lista3);
