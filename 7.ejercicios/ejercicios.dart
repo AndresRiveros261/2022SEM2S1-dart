a152e5c2705d74d4972031fd5626a098a15dbcdb
Andres Ricardo Riveros <104392170+AndresRiveros261@users.noreply.github.com>
2022-09-09 17:04:02 -0500

    Update ejercicios.dart

--- 7.ejercicios/ejercicios.dart
+++ 7.ejercicios/ejercicios.dart
@@ -21,19 +21,18 @@ final listaFinal =
   List.fold<Set>(List.first.toSet(), (a, b) => a.intersection(b.toSet())); 
 print(listaFinal);
 
-}
 
 //ejercicio 1
 
-var lista = List.of({1, 4, 9, 16, 25, 36, 49, 64, 81, 100});
+var lista = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
 var listapar = lista.where((number) => number.isEven);
 print(listapar);
 
 
 // ejercicio 2
 
-var lista1 = List.of({1,1,2,3,5,8,13,21,34,55,89});
-var lista2 = List.of({1,2,3,4,5,6,7,8,9,10,11,12,13});
+var lista1 = [1,1,2,3,5,8,13,21,34,55,89];
+var lista2 = [1,2,3,4,5,6,7,8,9,10,11,12,13];
 var lista3 = [];
 
   for (var numero in lista1) {
@@ -45,3 +44,17 @@ var lista3 = [];
   }
 
   print(lista3);
+
+
+// ejercicio 4 Validar si una cadena es un palíndromo o no.
+  
+  String palabras = "ARENERA";
+  
+  String auxiliar = palabras.split(' ').join('').toLowerCase();
+  print(auxiliar == auxiliar.split('').reversed.join('') ? 'Es un palíndromo': 'No es palíndromo');
+
+
+
+
+}
+
